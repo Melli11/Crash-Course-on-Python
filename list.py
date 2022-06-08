dfd9a4bdbfa3e69c9b2b36efd54432c7c6a4652b
Melli11 <mingerez@gmail.com>
2022-06-08 01:32:00 -0300

    Ãlist comprension

--- list.py
+++ list.py
@@ -71,3 +71,16 @@ def check(num): ##funcion auxiliar para chequear paridad
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+# List Comprehensions
+#La función números_impares devuelve una lista de números impares entre 1 y n, ambos inclusive.
+#  Completa los espacios en blanco de la función, usando la comprensión de listas.
+# Sugerencia: recuerde que los contadores de lista y rango comienzan en 0 y terminan en el límite menos 1.
+
+def odd_numbers(n):
+	return [x for x in range (1,n+1,2)]
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1)) # Should print []
