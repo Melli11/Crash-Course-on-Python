b029acbd52ba96e23f1b48832fa3af22c47a9661
Melli11 <mingerez@gmail.com>
2022-06-08 01:14:02 -0300

    Listas casi ok

---
+++ list.py
@@ -0,0 +1,72 @@
+# What is a list
+# Usando el método de cadena "dividida" de la lección anterior, complete la función get_word para devolver la palabra {n} de una oración pasada.
+# Por ejemplo, get_word("Esta es una lección sobre listas", 4) 
+# debería devolver "lección", que es la cuarta palabra de esta oración. Sugerencia: recuerde que los índices de lista comienzan en 0, no en 1.
+
+def get_word(sentence, n):
+	# Only proceed if n is positive 
+	if n > 0:
+		words = sentence.split() #split retorna una lista de char: Ejemplo si la oracion es "This is a lesson about lists" => words = ['This','is', 'a','lesson','about','lists'] 
+		# Only proceed if n is not more than the number of words 
+		if n <= len(words): #solo considero los n menores a la cantidad de palabras.
+			return(words[n-1]) # considerar que los arrays inician en el indice 0
+	return("") # si el indice es negativo retorno un espacio 
+
+print(get_word("This is a lesson about lists", 4)) # Should print: lesson
+print(get_word("This is a lesson about lists", -4)) # Nothing
+print(get_word("Now we are cooking!", 1)) # Should print: Now
+print(get_word("Now we are cooking!", 5)) # Nothing
+
+
+#Modifying the Contentes of a Lista
+# La función skip_elements devuelve una lista que contiene todos los demás elementos de una lista de entrada, comenzando con el primer elemento.
+# Complete esta función para hacer eso, usando el bucle for para iterar a través de la lista de entrada.
+
+def skip_elements(elements):
+  # Initialize variables
+  new_list = []
+  i = 0
+  
+  # Iterate through the list
+  for elements in elements:
+    # Does this element belong in the resulting list?
+    if i % 2 == 0:
+      # Add this element to the resulting list
+      new_list.append(elements)
+    # Increment i
+    i += 1
+
+  return new_list
+
+#List and Tuples
+# Usemos tuplas para almacenar información sobre un archivo: su nombre, su tipo y su tamaño en bytes. 
+# Complete los espacios en blanco en este código para devolver el tamaño en kilobytes (un kilobyte son 1024 bytes) hasta con 2 decimales.
+
+def file_size(file_info):
+	name, typefile, size = file_info #ojo con la palabra reservada type
+	return("{:.2f}".format(size / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
+# Iterating over Lists
+# Pruebe la función de enumerar usted mismo en este ejercicio rápido. Complete la función skip_elements para devolver todos los demás elementos de la lista, 
+# esta vez usando la función de enumerate para verificar si un elemento está en una posición par o impar
+
+def skip_elements(elements):
+	index = 0
+	aux = []
+	for index,elem in enumerate (elements): ##recorro lista
+		if  check(index) == True: ##solo indices pares
+			aux.append(elements[index])
+	return aux
+
+def check(num):
+    if (num % 2) == 0:
+        return True
+    else:
+        return False
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
